33383ab9fea82c18e34053a48600b5a94768e0e1
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-06-05 14:51:48 +0530

    add logic to navigate authenticated users away from auth pages

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,8 +6,19 @@ import {
   CardContent,
 } from '@/components/ui/card'
 import { LoginForm } from '@/features/auth/components/login-form'
+import { auth } from '@/lib/auth'
+import { headers } from 'next/headers'
+import { redirect } from 'next/navigation'
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  })
+
+  if (session) {
+    return redirect('/dashboard')
+  }
+
   return (
     <div className='flex items-center justify-center w-full grow p-4 sm:px-6 lg:px-8'>
       <div className='w-full max-w-sm'>
