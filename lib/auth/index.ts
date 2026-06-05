70d8248b6e7e9f27c6b87076eef49f27193493f7
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-06-05 13:41:56 +0530

    add next-cookies plugin to set cookies from server actions

--- lib/auth/index.ts
+++ lib/auth/index.ts
@@ -1,9 +1,11 @@
 import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
 import { db } from '@/db'
+import { nextCookies } from 'better-auth/next-js'
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: 'pg',
   }),
+  plugins: [nextCookies()],
 })
