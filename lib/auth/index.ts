5d95fa5959e20ee90fd585eb133f644d4188c234
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-06-05 13:44:06 +0530

    enable email + password auth

--- lib/auth/index.ts
+++ lib/auth/index.ts
@@ -7,6 +7,9 @@ export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: 'pg',
   }),
+  emailAndPassword: {
+    enabled: true,
+  },
   session: {
     cookieCache: {
       enabled: true,
