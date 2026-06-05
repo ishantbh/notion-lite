1ad1be508367c1af7416d6e4acb91f222741cb5d
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-06-05 13:10:31 +0530

    add header

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist_Mono, Inter } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/lib/utils'
 import { ThemeProvider } from '@/providers/theme/theme-provider'
+import { Header } from '@/components/header'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
 
@@ -38,7 +39,9 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <Header />
+
+          <main>{children}</main>
         </ThemeProvider>
       </body>
     </html>
---
+++ components/header.tsx
@@ -0,0 +1,59 @@
+import { ThemeToggle } from '@/components/theme/theme-toggle'
+import { Button } from '@/components/ui/button'
+import { LogOutIcon } from 'lucide-react'
+import Link from 'next/link'
+
+export function Header() {
+  const isAuthenticated = false
+
+  return (
+    <header className='border-b'>
+      <div className='w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4'>
+        <div className='flex items-center justify-between gap-4'>
+          <Link href='/' className='block text-lg sm:text-xl font-semibold'>
+            Notion Lite
+          </Link>
+
+          <nav>
+            <ul className='flex items-center gap-2'>
+              {isAuthenticated ? (
+                <>
+                  <li>
+                    <Button variant='link' asChild>
+                      <Link href='/dashboard'>Dashboard</Link>
+                    </Button>
+                  </li>
+
+                  <li>
+                    <ThemeToggle />
+                  </li>
+
+                  <li>
+                    <Button variant='destructive' size='icon'>
+                      <LogOutIcon className='size-4' />
+                      <span className='sr-only'>Logout</span>
+                    </Button>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li>
+                    <Button asChild>
+                      <Link href='/login'>Login</Link>
+                    </Button>
+                  </li>
+
+                  <li className='hidden sm:block'>
+                    <Button variant='secondary' asChild>
+                      <Link href='/sign-up'>Sign Up</Link>
+                    </Button>
+                  </li>
+                </>
+              )}
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  )
+}
