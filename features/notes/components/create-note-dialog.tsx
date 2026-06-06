58848a14483b120189a0dd8ae03a1dbbfb74192e
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-06-06 13:19:11 +0530

    add dialog to create-note

---
+++ features/notes/components/create-note-dialog.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import { Button, buttonVariants } from '@/components/ui/button'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import { CreateNoteForm } from '@/features/notes/components/create-note-form'
+import { VariantProps } from 'class-variance-authority'
+import { PlusIcon } from 'lucide-react'
+import { useState } from 'react'
+
+type Props = React.ComponentProps<'button'> &
+  VariantProps<typeof buttonVariants>
+
+export function CreateNoteDialog({
+  className,
+  variant = 'default',
+  size = 'default',
+  ...props
+}: Props) {
+  const [open, setOpen] = useState(false)
+
+  function close() {
+    setOpen(false)
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button variant={variant} size={size} className={className} {...props}>
+          <PlusIcon />
+          <span>New Note</span>
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create</DialogTitle>
+          <DialogDescription>Create a new note</DialogDescription>
+        </DialogHeader>
+
+        <CreateNoteForm close={close} />
+      </DialogContent>
+    </Dialog>
+  )
+}
