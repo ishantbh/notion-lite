4160c3cc7d3a5e64a7ee8baa823261ccdd7e8a57
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-06-07 10:21:28 +0530

    update middleware and add strict session check to protect note details page

--- app/(app)/notes/[noteId]/page.tsx
+++ app/(app)/notes/[noteId]/page.tsx
@@ -1,3 +1,15 @@
-export default function Page() {
+import { auth } from '@/lib/auth'
+import { headers } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+export default async function Page() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  })
+
+  if (!session) {
+    redirect('/login')
+  }
+
   return <div>Note Page</div>
 }
