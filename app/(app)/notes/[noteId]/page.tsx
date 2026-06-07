2ddcc6fa80ad1b8ec288a682a2e6ad6968441391
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-06-07 10:40:12 +0530

    fetch note by id from params on note details page

--- app/(app)/notes/[noteId]/page.tsx
+++ app/(app)/notes/[noteId]/page.tsx
@@ -1,8 +1,15 @@
+import { db } from '@/db'
+import { notes } from '@/db/schema'
 import { auth } from '@/lib/auth'
+import { eq } from 'drizzle-orm'
 import { headers } from 'next/headers'
-import { redirect } from 'next/navigation'
+import { notFound, redirect } from 'next/navigation'
 
-export default async function Page() {
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ noteId: string }>
+}) {
   const session = await auth.api.getSession({
     headers: await headers(),
   })
@@ -11,5 +18,15 @@ export default async function Page() {
     redirect('/login')
   }
 
-  return <div>Note Page</div>
+  const { noteId } = await params
+
+  const note = await db.query.notes.findFirst({
+    where: eq(notes.id, noteId),
+  })
+
+  if (!note) {
+    notFound()
+  }
+
+  return <div>{note.title}</div>
 }
