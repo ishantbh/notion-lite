86d2f9526a4b6925ea8a3c65e99b0cba831245a6
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-06-09 00:31:46 +0530

    add types for tags

--- db/types.ts
+++ db/types.ts
@@ -1,4 +1,7 @@
-import { notes } from '@/db/schema'
+import { notes, tags } from '@/db/schema'
 
 export type Note = typeof notes.$inferSelect
 export type NewNote = typeof notes.$inferInsert
+
+export type Tag = typeof tags.$inferSelect
+export type NewTag = typeof tags.$inferInsert
