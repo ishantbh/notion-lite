37ad625eb3f0c496ab44edc14681c49448a4b541
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-06-11 22:05:13 +0530

    add sidebar footer component

---
+++ components/app-sidebar/app-sidebar-footer.tsx
@@ -0,0 +1,110 @@
+'use client'
+
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import {
+  SidebarFooter,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSkeleton,
+  useSidebar,
+} from '@/components/ui/sidebar'
+import { authClient } from '@/lib/auth/auth-client'
+import { ChevronsUpDown, LogOutIcon } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import { toast } from 'sonner'
+
+export function AppSidebarFooter() {
+  const router = useRouter()
+  const { data: session, isPending } = authClient.useSession()
+
+  const { isMobile } = useSidebar()
+
+  if (!session) {
+    return null
+  }
+
+  async function handleLogout() {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          toast.success('Logout successful')
+          router.push('/login')
+        },
+        onError: (ctx) => {
+          toast.error(ctx.error.message)
+        },
+      },
+    })
+  }
+
+  return (
+    <SidebarFooter>
+      <SidebarMenu>
+        <SidebarMenuItem>
+          {isPending ? (
+            <SidebarMenuSkeleton className='h-8' />
+          ) : (
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton
+                  size='lg'
+                  className='data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground'
+                >
+                  <AvatarExtended />
+                  <ChevronsUpDown className='ml-auto size-4' />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                className='w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg'
+                side={isMobile ? 'bottom' : 'right'}
+                align='end'
+                sideOffset={4}
+              >
+                <DropdownMenuLabel className='p-0 font-normal'>
+                  <div className='flex items-center gap-2 px-1 py-1.5 text-left text-sm'>
+                    <AvatarExtended />
+                  </div>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem variant='destructive' onClick={handleLogout}>
+                  <LogOutIcon />
+                  <span>Log out</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          )}
+        </SidebarMenuItem>
+      </SidebarMenu>
+    </SidebarFooter>
+  )
+}
+
+function AvatarExtended() {
+  const { data: session } = authClient.useSession()
+
+  if (!session) return null
+
+  const { user } = session
+
+  return (
+    <>
+      <Avatar className='h-8 w-8 rounded-lg'>
+        <AvatarImage src={user.image ?? undefined} alt={user.name} />
+        <AvatarFallback>{user.name[0]}</AvatarFallback>
+      </Avatar>
+      <div className='grid flex-1 text-left text-sm leading-tight'>
+        <span className='truncate font-medium'>{user.name}</span>
+        <span className='truncate text-xs'>{user.email}</span>
+      </div>
+    </>
+  )
+}
