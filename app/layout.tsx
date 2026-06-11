1f47b65d86f5966d5c809dbfde1b25a6ae2865e9
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-06-11 21:25:22 +0530

    wrap the whole app with tooltip-provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
+import { Toaster } from '@/components/ui/sonner'
+import { TooltipProvider } from '@/components/ui/tooltip'
+import { cn } from '@/lib/utils'
+import { ThemeProvider } from '@/providers/theme/theme-provider'
 import type { Metadata } from 'next'
 import { Geist_Mono, Inter } from 'next/font/google'
 import './globals.css'
-import { cn } from '@/lib/utils'
-import { ThemeProvider } from '@/providers/theme/theme-provider'
-import { Toaster } from '@/components/ui/sonner'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
 
@@ -39,7 +40,9 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <main className='flex grow'>{children}</main>
+          <TooltipProvider>
+            <main className='flex grow'>{children}</main>
+          </TooltipProvider>
 
           <Toaster />
         </ThemeProvider>
