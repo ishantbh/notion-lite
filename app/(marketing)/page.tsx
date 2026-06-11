1beb9ac00daaf52a0b480e35c011ed9dd76ed51c
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-06-11 20:47:59 +0530

    update root layout, moving header to landing page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,10 +1,15 @@
+import { Header } from '@/components/header'
 import { ThemeToggle } from '@/components/theme/theme-toggle'
 
 export default function Page() {
   return (
-    <div>
-      <h1 className='text-2xl font-bold'>Notion Lite</h1>
-      <ThemeToggle />
+    <div className='w-full'>
+      <Header />
+
+      <div>
+        <h1 className='text-2xl font-bold'>Notion Lite</h1>
+        <ThemeToggle />
+      </div>
     </div>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist_Mono, Inter } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/lib/utils'
 import { ThemeProvider } from '@/providers/theme/theme-provider'
-import { Header } from '@/components/header'
 import { Toaster } from '@/components/ui/sonner'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' })
@@ -40,8 +39,6 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <Header />
-
           <main className='flex grow'>{children}</main>
 
           <Toaster />
