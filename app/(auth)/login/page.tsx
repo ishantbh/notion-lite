536aa014ce55b9c5377fe77e2aec80e681ada5f5
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-06-11 20:56:20 +0530

    add link on auth pages to navigate back to landing page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,13 +1,14 @@
 import {
   Card,
+  CardContent,
+  CardDescription,
   CardHeader,
   CardTitle,
-  CardDescription,
-  CardContent,
 } from '@/components/ui/card'
 import { LoginForm } from '@/features/auth/components/login-form'
 import { auth } from '@/lib/auth'
 import { headers } from 'next/headers'
+import Link from 'next/link'
 import { redirect } from 'next/navigation'
 
 export default async function Page() {
@@ -20,7 +21,11 @@ export default async function Page() {
   }
 
   return (
-    <div className='flex items-center justify-center w-full grow p-4 sm:px-6 lg:px-8'>
+    <div className='flex flex-col items-center justify-center w-full grow p-4 sm:px-6 lg:px-8'>
+      <Link href='/' className='text-xl sm:text-2xl font-semibold mb-6'>
+        Notion Lite
+      </Link>
+
       <div className='w-full max-w-sm'>
         <Card>
           <CardHeader className='text-center'>
