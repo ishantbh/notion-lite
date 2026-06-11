880bbcc9c1e9fec4509c2eb590fbbc86ee038517
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-06-11 22:06:21 +0530

    add sidebar rail

--- components/app-sidebar/app-sidebar.tsx
+++ components/app-sidebar/app-sidebar.tsx
@@ -1,13 +1,13 @@
 import { AppSidebarFooter } from '@/components/app-sidebar/app-sidebar-footer'
 import { AppSidebarHeader } from '@/components/app-sidebar/app-sidebar-header'
-import { Sidebar } from '@/components/ui/sidebar'
+import { Sidebar, SidebarRail } from '@/components/ui/sidebar'
 
 export function AppSidebar() {
   return (
     <Sidebar>
       <AppSidebarHeader />
-
       <AppSidebarFooter />
+      <SidebarRail />
     </Sidebar>
   )
 }
