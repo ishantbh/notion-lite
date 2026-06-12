2491f20a977c106f02678c75f2d5be54639d5889
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-06-12 14:06:02 +0530

    cleanup unused query get-tags

--- features/notes/data/get-tags.ts
+++
@@ -1,9 +0,0 @@
-import { db } from '@/db'
-import { tags } from '@/db/schema'
-import { eq } from 'drizzle-orm'
-
-export async function getTags(userId: string) {
-  return db.query.tags.findMany({
-    where: eq(tags.userId, userId),
-  })
-}
