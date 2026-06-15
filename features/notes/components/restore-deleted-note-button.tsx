3f863dfc28af4523b56b166301b25598940b2bfd
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-06-16 00:04:41 +0530

    add dummy button to restore deleted note

--- features/notes/components/restore-deleted-note-button.tsx
+++ features/notes/components/restore-deleted-note-button.tsx
@@ -1,7 +1,19 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+
 type Props = {
   noteId: string
 }
 
 export function RestoreDeletedNoteButton({ noteId }: Props) {
-  return <div>RestoreDeletedNoteButton</div>
+  function handleRestoreDeletedNote() {
+    // TODO: Restore deleted note
+  }
+
+  return (
+    <Button variant='outline' onClick={handleRestoreDeletedNote}>
+      Restore
+    </Button>
+  )
 }
