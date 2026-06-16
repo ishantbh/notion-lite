7b36fec23ba20ac7bc4d763b25d3385862c5dbf7
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-06-16 13:21:33 +0530

    improve sidebar nav to visually indicate active page

--- components/app-sidebar/app-sidebar-nav.tsx
+++ components/app-sidebar/app-sidebar-nav.tsx
@@ -1,54 +1,59 @@
+'use client'
+
 import {
   SidebarGroup,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
 } from '@/components/ui/sidebar'
-import { Heart, NotebookTextIcon, Settings, Trash } from 'lucide-react'
+import { cn } from '@/lib/utils'
+import {
+  NotebookTextIcon,
+  SettingsIcon,
+  StarIcon,
+  Trash2Icon,
+} from 'lucide-react'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+const sidebarNavItems = [
+  { title: 'Dashboard', href: '/dashboard', icon: NotebookTextIcon },
+  { title: 'Starred', href: '/starred', icon: StarIcon },
+  { title: 'Settings', href: '/settings', icon: SettingsIcon },
+  { title: 'Trash', href: '/trash', icon: Trash2Icon, destructive: true },
+]
 
 export function AppSidebarNav() {
+  const pathname = usePathname()
+
   return (
     <SidebarGroup>
       <SidebarMenu>
-        <SidebarMenuItem>
-          <SidebarMenuButton isActive asChild>
-            <Link href='/dashboard'>
-              <NotebookTextIcon />
-              <span>Dashboard</span>
-            </Link>
-          </SidebarMenuButton>
-        </SidebarMenuItem>
+        {sidebarNavItems.map((item) => {
+          const Icon = item.icon
+          const isActive = pathname.startsWith(item.href)
 
-        <SidebarMenuItem>
-          <SidebarMenuButton asChild>
-            <Link href='/favorites'>
-              <Heart />
-              <span>Favorites</span>
-            </Link>
-          </SidebarMenuButton>
-        </SidebarMenuItem>
-
-        <SidebarMenuItem>
-          <SidebarMenuButton asChild>
-            <Link href='/settings'>
-              <Settings />
-              <span>Settings</span>
-            </Link>
-          </SidebarMenuButton>
-        </SidebarMenuItem>
-
-        <SidebarMenuItem>
-          <SidebarMenuButton
-            className='text-red-300 hover:text-red-300'
-            asChild
-          >
-            <Link href='/trash'>
-              <Trash />
-              <span>Trash</span>
-            </Link>
-          </SidebarMenuButton>
-        </SidebarMenuItem>
+          return (
+            <SidebarMenuItem key={item.href}>
+              <SidebarMenuButton
+                asChild
+                isActive={isActive}
+                className={cn({
+                  'data-active:text-destructive hover:text-destructive active:text-destructive data-active:bg-destructive/10 hover:bg-destructive/10 active:bg-destructive/10':
+                    item.destructive,
+                })}
+              >
+                <Link
+                  href={item.href}
+                  className={cn({ 'text-destructive': item.destructive })}
+                >
+                  <Icon />
+                  <span>{item.title}</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          )
+        })}
       </SidebarMenu>
     </SidebarGroup>
   )
