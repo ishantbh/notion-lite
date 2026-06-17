62d63e5c5b886bba38c344281f8ddae83628facf
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-06-17 14:17:20 +0530

    update types to be more reliable, introducing new tag with creatable for tag-picker

--- features/tags/types.ts
+++ features/tags/types.ts
@@ -1,9 +1,13 @@
-export type TagListItem = {
-  id: string
-  name: string
+import { Tag } from '@/db/types'
+
+export type TagItem = Pick<Tag, 'id' | 'name'>
+
+export type TagListItem = TagItem & {
   notesCount: number
 }
 
 export type GetTagsResponse = {
   tags: TagListItem[]
 }
+
+export type TagPickerItem = TagItem & { creatable?: string }
