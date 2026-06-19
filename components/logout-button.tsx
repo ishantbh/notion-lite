27ce4acdd4a34f900e3511e849f53e3f5ac23585
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-06-19 13:44:29 +0530

    clear cache on logout

--- components/logout-button.tsx
+++ components/logout-button.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from '@/components/ui/button'
 import { authClient } from '@/lib/auth/auth-client'
+import { useQueryClient } from '@tanstack/react-query'
 import { LogOutIcon } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 import { toast } from 'sonner'
@@ -9,10 +10,13 @@ import { toast } from 'sonner'
 export function LogoutButton() {
   const router = useRouter()
 
+  const queryClient = useQueryClient()
+
   async function handleLogout() {
     await authClient.signOut({
       fetchOptions: {
         onSuccess: () => {
+          queryClient.clear()
           toast.success('Logout successful')
           router.push('/login')
         },
