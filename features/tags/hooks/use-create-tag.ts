d295a16e153158942bfc093e98caa1a63a246e85
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-06-19 13:29:48 +0530

    cleanup create-tag mutation to only manage cache, rather than displaying toasts

--- features/tags/hooks/use-create-tag.ts
+++ features/tags/hooks/use-create-tag.ts
@@ -1,6 +1,5 @@
 import { createTag } from '@/features/tags/server/create-tag'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
-import { toast } from 'sonner'
 
 export function useCreateTag() {
   const queryClient = useQueryClient()
@@ -8,12 +7,7 @@ export function useCreateTag() {
   return useMutation({
     mutationFn: createTag,
     onSuccess: async () => {
-      toast.success('Tag created successfully')
-
       await queryClient.invalidateQueries({ queryKey: ['tags'] })
-    },
-    onError: (error) => {
-      toast.error(error.message)
     },
   })
 }
