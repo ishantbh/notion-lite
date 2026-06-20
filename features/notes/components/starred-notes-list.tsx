632fa7e0182235563b5e2709ccff9f22bedf51fd
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-06-20 08:44:58 +0530

    display pagination component on starred-notes page

--- features/notes/components/starred-notes-list.tsx
+++ features/notes/components/starred-notes-list.tsx
@@ -1,31 +1,50 @@
 import { db } from '@/db'
 import { notes } from '@/db/schema'
 import { NotesItem } from '@/features/notes/components/dashboard/notes-item'
-import { and, desc, eq } from 'drizzle-orm'
+import { NotesPagination } from '@/features/notes/components/dashboard/notes-pagination'
+import { NOTES_PER_PAGE } from '@/lib/utils'
+import { and, count, desc, eq } from 'drizzle-orm'
 import { FolderOpenIcon } from 'lucide-react'
 
 type Props = {
   userId: string
+  currentPage: number
 }
 
-export async function StarredNotesList({ userId }: Props) {
-  const userStarredNotes = await db.query.notes.findMany({
-    with: {
-      noteTags: {
-        with: {
-          tag: true,
+export async function StarredNotesList({ userId, currentPage }: Props) {
+  const [userStarredNotes, [{ total }]] = await Promise.all([
+    db.query.notes.findMany({
+      with: {
+        noteTags: {
+          with: {
+            tag: true,
+          },
         },
       },
-    },
-    where: and(
-      eq(notes.userId, userId),
-      eq(notes.isDeleted, false),
-      eq(notes.isStarred, true),
-    ),
-    orderBy: desc(notes.updatedAt),
-  })
+      where: and(
+        eq(notes.userId, userId),
+        eq(notes.isDeleted, false),
+        eq(notes.isStarred, true),
+      ),
+      orderBy: desc(notes.updatedAt),
+      limit: NOTES_PER_PAGE,
+      offset: (currentPage - 1) * NOTES_PER_PAGE,
+    }),
+    db
+      .select({ total: count() })
+      .from(notes)
+      .where(
+        and(
+          eq(notes.userId, userId),
+          eq(notes.isDeleted, false),
+          eq(notes.isStarred, true),
+        ),
+      ),
+  ])
 
-  if (userStarredNotes.length === 0) {
+  const totalPages = Math.ceil(total / NOTES_PER_PAGE)
+
+  if (totalPages === 0) {
     return (
       <div className='flex flex-col items-center justify-center text-center gap-4 grow opacity-80'>
         <FolderOpenIcon className='size-16 text-muted-foreground' />
@@ -38,10 +57,14 @@ export async function StarredNotesList({ userId }: Props) {
   }
 
   return (
-    <ul className='mt-8 grid grid-cols-1 gap-4 sm:gap-6 sm:grid-cols-2 lg:grid-cols-3'>
-      {userStarredNotes.map((note) => (
-        <NotesItem key={note.id} note={note} />
-      ))}
-    </ul>
+    <div className='flex flex-col gap-4 grow justify-between'>
+      <ul className='mt-8 grid grid-cols-1 gap-4 sm:gap-6 sm:grid-cols-2 lg:grid-cols-3'>
+        {userStarredNotes.map((note) => (
+          <NotesItem key={note.id} note={note} />
+        ))}
+      </ul>
+
+      <NotesPagination totalPages={totalPages} />
+    </div>
   )
 }
