cf5b689204f3251ce259a57fb922a5e5ac4b0756
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-06-20 12:59:29 +0530

    add search component

---
+++ features/notes/components/dashboard/notes-search.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import { SearchIcon } from 'lucide-react'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { useDebouncedCallback } from 'use-debounce'
+
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from '@/components/ui/input-group'
+
+export function NotesSearch({ count }: { count: number }) {
+  const searchParams = useSearchParams()
+  const { replace } = useRouter()
+  const pathname = usePathname()
+
+  const handleSearch = useDebouncedCallback((term: string) => {
+    const params = new URLSearchParams(searchParams)
+
+    params.set('page', '1')
+
+    if (term) {
+      params.set('query', term)
+    } else {
+      params.delete('query')
+    }
+    replace(`${pathname}?${params.toString()}`)
+  }, 300)
+
+  return (
+    <InputGroup className='w-full max-w-md mx-auto'>
+      <InputGroupInput
+        defaultValue={searchParams.get('query')?.toString()}
+        placeholder='Search...'
+        onChange={(e) => {
+          handleSearch(e.target.value)
+        }}
+      />
+      <InputGroupAddon>
+        <SearchIcon />
+      </InputGroupAddon>
+      <InputGroupAddon align='inline-end'>
+        {count} {count === 1 ? 'result' : 'results'}
+      </InputGroupAddon>
+    </InputGroup>
+  )
+}
