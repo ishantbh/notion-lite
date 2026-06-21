36818ce73cd7beadd6f55a479d3dd5eb59e02902
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-06-21 23:48:43 +0530

    add theme toggle on settings page

--- app/(app)/settings/page.tsx
+++ app/(app)/settings/page.tsx
@@ -1,4 +1,6 @@
 import { HeaderWithSidebar } from '@/components/header-with-sidebar'
+import { ThemeToggle } from '@/components/theme/theme-toggle'
+import { Separator } from '@/components/ui/separator'
 import { auth } from '@/lib/auth'
 import { headers } from 'next/headers'
 import { redirect } from 'next/navigation'
@@ -16,7 +18,16 @@ export default async function Page() {
     <div className='flex flex-col grow'>
       <HeaderWithSidebar title='Settings' showSearch={false} />
 
-      <main className='w-full max-w-7xl mx-auto px-4 flex flex-col grow'></main>
+      <main className='w-full max-w-3xl mx-auto px-4 flex flex-col grow'>
+        <div className='flex flex-col w-full mt-8'>
+          <div className='flex items-center justify-between gap-4 w-full px-4'>
+            <h2 className='text-lg font-semibold'>Theme</h2>
+            <ThemeToggle />
+          </div>
+
+          <Separator className='my-4' />
+        </div>
+      </main>
     </div>
   )
 }
