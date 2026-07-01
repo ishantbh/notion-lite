44fedfa7e0e8585fed2c749fb06e5642bec1d239
Ishant B <269895395+ishantbh@users.noreply.github.com>
2026-07-01 13:16:45 +0530

    improve logout-button to accept prop to display/hide title

--- components/logout-button.tsx
+++ components/logout-button.tsx
@@ -1,36 +1,45 @@
-'use client'
+"use client";
 
-import { Button } from '@/components/ui/button'
-import { authClient } from '@/lib/auth/auth-client'
-import { useQueryClient } from '@tanstack/react-query'
-import { LogOutIcon } from 'lucide-react'
-import { useRouter } from 'next/navigation'
-import { toast } from 'sonner'
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth/auth-client";
+import { cn } from "@/lib/utils";
+import { useQueryClient } from "@tanstack/react-query";
+import { LogOutIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
-export function LogoutButton() {
-  const router = useRouter()
+type Props = {
+  withTitle?: boolean;
+};
 
-  const queryClient = useQueryClient()
+export function LogoutButton({ withTitle = false }: Props) {
+  const router = useRouter();
+
+  const queryClient = useQueryClient();
 
   async function handleLogout() {
     await authClient.signOut({
       fetchOptions: {
         onSuccess: () => {
-          queryClient.clear()
-          toast.success('Logout successful')
-          router.push('/login')
+          queryClient.clear();
+          toast.success("Logout successful");
+          router.push("/login");
         },
         onError: (ctx) => {
-          toast.error(ctx.error.message)
+          toast.error(ctx.error.message);
         },
       },
-    })
+    });
   }
 
   return (
-    <Button variant='destructive' size='icon' onClick={handleLogout}>
-      <LogOutIcon className='size-4' />
-      <span className='sr-only'>Logout</span>
+    <Button
+      variant="destructive"
+      size={withTitle ? "sm" : "icon"}
+      onClick={handleLogout}
+    >
+      <LogOutIcon className="size-4" />
+      <span className={cn({ "sr-only": !withTitle })}>Logout</span>
     </Button>
-  )
+  );
 }
