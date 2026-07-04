4b06633f4c83326370d488cc94f305d8df91f5cb
Ishant B <269895395+ishantbh@users.noreply.github.com>
2026-07-04 11:46:57 +0530

    add header to create-note page

--- app/(app)/notes/create/page.tsx
+++ app/(app)/notes/create/page.tsx
@@ -1,3 +1,4 @@
+import { HeaderWithSidebar } from '@/components/header-with-sidebar'
 import { CreateEditNoteForm } from '@/features/notes/components/create-edit-note-form'
 import { auth } from '@/lib/auth'
 import { headers } from 'next/headers'
@@ -13,19 +14,23 @@ export default async function Page() {
   }
 
   return (
-    <div className='w-full max-w-7xl mx-auto p-4 sm:p-6 lg:px-8 flex flex-col grow'>
-      <div className='flex items-center gap-4 justify-between'>
-        <div className='space-y-1'>
-          <h1 className='text-xl sm:text-2xl font-semibold'>Create Note</h1>
-          <p className='text-sm sm:text-base text-muted-foreground'>
-            Create a new note
-          </p>
-        </div>
-      </div>
+    <div className='flex flex-col grow'>
+      <HeaderWithSidebar title='Create Note' showSearch={false} />
 
-      <div className='mt-6'>
-        <CreateEditNoteForm />
-      </div>
+      <main className='w-full max-w-7xl mx-auto p-4 sm:p-6 lg:px-8 flex flex-col grow'>
+        <div className='flex items-center gap-4 justify-between'>
+          <div className='space-y-1'>
+            <h1 className='text-xl sm:text-2xl font-semibold'>Create Note</h1>
+            <p className='text-sm sm:text-base text-muted-foreground'>
+              Create a new note
+            </p>
+          </div>
+        </div>
+
+        <div className='mt-6'>
+          <CreateEditNoteForm />
+        </div>
+      </main>
     </div>
   )
 }
