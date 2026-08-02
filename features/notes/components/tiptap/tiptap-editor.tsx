8565a2b475c03808a6fc4f62ba8be2545ba99329
Ishant B <269895395+ishantbh@users.noreply.github.com>
2026-08-02 23:49:42 +0530

    update tiptap editor to accept default value and basic text onChange event

--- features/notes/components/tiptap/tiptap-editor.tsx
+++ features/notes/components/tiptap/tiptap-editor.tsx
@@ -5,12 +5,24 @@ import StarterKit from '@tiptap/starter-kit'
 
 import { Skeleton } from '@/components/ui/skeleton'
 
-export default function TiptapEditor() {
+export default function TiptapEditor({
+  value,
+  onChange,
+  disabled = false,
+}: {
+  value?: string
+  onChange: (value: string) => void
+  disabled?: boolean
+}) {
   const editor = useEditor({
     extensions: [StarterKit],
-    content: '<p>Hello World! 🌎️</p>',
+    content: value ?? '',
     // Don't render immediately on the server to avoid SSR issues
     immediatelyRender: false,
+    editable: !disabled,
+    onUpdate: ({ editor }) => {
+      onChange(editor.getText())
+    },
   })
 
   if (!editor) return <Skeleton className='w-full h-16' />
