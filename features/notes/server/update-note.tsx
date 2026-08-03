1fad713dfc4ad3fab8fd4a3f01e88997fcbae37c
Ishant B <269895395+ishantbh@users.noreply.github.com>
2026-08-03 11:54:59 +0530

    update update-note server action to generate plain text from json

--- features/notes/server/update-note.tsx
+++ features/notes/server/update-note.tsx
@@ -7,6 +7,8 @@ import {
   type CreateEditNoteSchema,
 } from '@/features/notes/schemas/create-edit-note-schema'
 import { auth } from '@/lib/auth'
+import { generateText } from '@tiptap/core'
+import { StarterKit } from '@tiptap/starter-kit'
 import { and, eq } from 'drizzle-orm'
 import { headers } from 'next/headers'
 
@@ -29,10 +31,12 @@ export async function updateNote(noteId: string, data: CreateEditNoteSchema) {
 
   const { title, content, tags } = parsed.data
 
+  const contentText = generateText(content, [StarterKit])
+
   await Promise.all([
     db
       .update(notes)
-      .set({ title, content })
+      .set({ title, content, contentText })
       .where(and(eq(notes.id, noteId), eq(notes.userId, userId)))
       .returning({
         id: notes.id,
